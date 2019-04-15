b41155dbbbc3726cb28f2ef55540e129d9ec8bca
MarcosInfantino <49456888+MarcosInfantino@users.noreply.github.com>
2019-04-15 17:47:59 -0300

    Add files via upload

--- Tp individual 1.hs
+++ Tp individual 1.hs
@@ -1,4 +1,5 @@
 --type pirata=(nombre,lista)
+--tripulacion (saqueo, [pirata])
 jack=("jack",[("llavero",10000),("moneda",6)])
 lst=[("0",5),("0",6)]
 cantidadDeTesoros (nombre,lista)=length lista
@@ -9,7 +10,7 @@ listaValores lista= map valor lista
 pirataAfortunado::(String,[(String, Int)])->Bool
 pirataAfortunado(nombre,lista)= (sum.listaValores) lista>10000
 
---mismoValorDif (x,y) (z,w)= x==z && y!=w
+mismoValorDif (x,y) (z,w)= x==z && y/=w
 
 
 valorTesoroMasValioso ::(String,[(String, Int)]) ->Int
@@ -20,19 +21,24 @@ tesoroNoValioso::(String,Int)->Bool
 tesoroNoValioso (x,y)=y<100
 perdioValiosos (nombre,lista)= (nombre,filter (tesoroNoValioso) lista)
 
-nombreDado::(String, Int)-> String-> Bool
-nombreDado (x,y) z= x==z
+
 perdioNombre::(String,[(String, Int)])->String->(String,[(String, Int)])
---(filter(!=(nom,- )))lista
---perdioNombre (nombre,lista) nom=(nombre, filter(!=(nom,-))lista)
+perdioNombre (nombre,lista) nom=(nombre, filter((/=nom).fst)lista)
 
 tesoroValioso (x,y)=y>100
 filtrarValioso::(String, Int)-> [(String, Int)]
 filtrarValioso t=filter( tesoroValioso)[t]
-saquear:: String-> (String,[(String, Int)])->(String,Int)-> (String,[(String, Int)])
+--saquear:: String-> (String,[(String, Int)])->(String,Int)-> (String,[(String, Int)])
 saquear "valor" (nombre,lista) tesoro = (nombre,lista ++ (filtrarValioso tesoro))
 
 saquear "amor" (nombre,lista) tesoro= (nombre,lista)
---tesoroCorrecto (x,y) z= x==z
---filtrarTesoroCorrecto t obj= filter(tesoroCorrecto t obj)[t]
---saquear objeto (nombre,lista) tesoro= (nombre,lista ++ (filtrarTesoroCorrecto tesoro objeto))
+
+saquear objeto (nombre,lista) tesoro= (nombre,lista ++ (filter((==objeto).fst)[tesoro]))
+
+--saquear ("amor","valor") (nombre,lista) tesoro = (nombre,lista ++ (filtrarValioso tesoro))
+--saquear  "valor" "amor" (nombre,lista) tesoro = (nombre,lista ++ (filtrarValioso tesoro))
+--saquear "amor" objeto (nombre,lista) tesoro= (nombre,lista ++ (filter((==objeto).fst)[tesoro]))
+--saquear "valor" objeto (nombre,lista) tesoro=(nombre,lista ++[ fst ((filter((==objeto).fst)[tesoro])++ (filtrarValioso tesoro))])
+
+nuevoPirata (saqueo, listaPiratas) pirata=(saqueo, listaPiratas ++ [pirata])
+sacarPirata (saqueo, listaPiratas) nom=(saqueo, filter((/=nom).fst))
